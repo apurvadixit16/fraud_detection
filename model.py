3a2c50b72deceffb9f4e06ed0071d70a9e7a59d8
apurvadixit16 <apurva1603@gmail.com>
2022-05-26 16:18:50 +0530

    added 3 new ml model code

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
 print("Adding all ML related code here")
+
+
+def model():
+    print("adding code for ML Model1")
+
+def model():
+    print("adding code for ML Model2")
+
+def model():
+    print("adding code for ML Model3")
